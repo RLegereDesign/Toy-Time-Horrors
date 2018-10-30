62882ca7cacfaa68a8aeb81218e8ed004d1dbad0
Unknown <rlegeredesign@gmail.com>
2018-10-30 19:04:11 -0400

    Initial Project Upload

---
+++ TheMediumestGAme/Assets/FPSLook.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLook : MonoBehaviour {
+
+
+	public enum RotationAxes {MouseXAndY = 0, MouseX = 1, MouseY = 2}
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+
+	public float minX = -360F;
+	public float maxX = 360F;
+
+	public float minY = -60F;
+	public float maxY = 60F;
+
+	float rotationY = 0F;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (axes == RotationAxes.MouseXAndY) {
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * sensitivityX;
+
+			rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minY, maxY);
+
+			transform.localEulerAngles = new Vector3 (-rotationY, rotationX, 0);
+		} else if (axes == RotationAxes.MouseX) {
+			transform.Rotate (0, Input.GetAxis ("Mouse X") * sensitivityX, 0);
+		} else {
+
+			rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minY, maxY);
+
+			transform.localEulerAngles = new Vector3 (-rotationY, transform.localEulerAngles.y, 0);
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+		
+	}
+}
