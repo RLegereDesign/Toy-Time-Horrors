ac3b9699a0f71ee81f51424c1d7be8ec3ecbe841
Unknown <rlegeredesign@gmail.com>
2018-10-31 04:43:45 -0400

    Alpha Programming/Graybox
    Alpha is 90% programmed and grayboxed

---
+++ TheMediumestGAme/Assets/DartForce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartForce : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float DartSpeed;
+
+
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(Vector3.forward * DartSpeed);
+
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+}
---
+++ TheMediumestGAme/Assets/DartShot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartShot : MonoBehaviour {
+    public GameObject Dart;
+    public GameObject Spawner;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(Dart, Spawner.transform.position, gameObject.transform.rotation);
+
+
+        }
+		
+	}
+}
